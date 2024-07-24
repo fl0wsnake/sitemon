ccb95f4a858c1c25ce057d9c905f95293156a9f9
Cyril Krylov <cyrilo.krylov@gmail.com>
2024-07-24 12:55:09 +0300

    .

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/gocolly/colly/v2"
-
+	"github.com/PuerkitoBio/goquery"
+	// "github.com/gocolly/colly/v2"
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
@@ -31,30 +32,36 @@ func main() {
 	}
 
 	for _, configEntry := range config {
-		c := colly.NewCollector()
 
-		c.OnHTML(configEntry.Sel,
-			func(e *colly.HTMLElement) {
-				fmt.Println(e.Text)
-			})
+		response, err := http.Get(configEntry.Site)
+		if err != nil {
+			log.Fatal(err)
+		}
+		doc, err := goquery.NewDocumentFromReader(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(doc.Find(configEntry.Sel).Text())
 
-		c.Visit(configEntry.Site)
-		c.OnResponse(func (r colly.Response)  {
-			r.Headers.Get("") // TODO: title
-		})
+		// c := colly.NewCollector()
+		// c.OnHTML(configEntry.Sel,
+		// 	func(e *colly.HTMLElement) {
+		// 		fmt.Println(e.Text)
+		// 	})
+		// c.OnResponse(func(r *colly.Response) {
+		// 	title := string(r.Body)
+		// 	fmt.Println("title: " + title)
+		// })
+		// c.Visit(configEntry.Site)
+
 	}
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
 }
 
 func sendEmail(to, subject, content, link string) {
 	fromEmail := mail.NewEmail("Tira", "tiramisu@example.com")
 	toEmail := mail.NewEmail(to, to)
 	htmlContent := fmt.Sprintf(`%s<a href="%s">`, content, link)
-	message := mail.NewSingleEmail(fromEmail, subject, toEmail, plainTextContent, htmlContent)
+	message := mail.NewSingleEmail(fromEmail, subject, toEmail, content, htmlContent)
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	response, err := client.Send(message)
 	if err != nil {
@@ -65,7 +72,7 @@ func sendEmail(to, subject, content, link string) {
 	}
 }
 
-// // TODO: init
+// // TODO: cloud function init
 // func main() {
 //    functions.HTTP("HelloHTTP", helloHTTP)
 // }
